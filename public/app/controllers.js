2a61d2ffab23ba656f96ce4df3045bb568fbcbaf
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-07-31 12:24:55 -0700

    create functionality

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,9 +1,27 @@
-angular.module('AirplaneCtrls', [])
+angular.module('AirplaneCtrls', ['AirplaneServices'])
 
 .controller('TakeOffCtrl', ['$scope', function($scope) {
 	$scope.captain = "Captain Mike Dexter";
 }])
 
-.controller('InventoryCtrl', ['$scope', function($scope) {
-	$scope.planes = [747, 737, 787]
+.controller('InventoryCtrl', ['$scope', 'AirplaneAPI', function($scope, AirplaneAPI) {
+	$scope.planes = [747, 737, 787];
+
+	AirplaneAPI.query(function success(data){
+		console.log("success: ", data);
+		$scope.planes = data;
+	}, function error(data) {
+		console.log("error: ", data);
+	});
+	$scope.createPlane = function(manufacturer, model, engines) {
+		AirplaneAPI.save({
+			manufacturer: manufacturer,
+			model: model,
+			engines: engines
+		}, function success(plane) {
+			$scope.planes.push(plane);
+		}, function error() {
+			console.log("error: ", data);
+		})
+	}
 }])
